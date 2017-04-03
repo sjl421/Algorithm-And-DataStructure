a11b4151795c9b6b0cb580d5956d2afc915d31d3
LazyCoder <553899811@qq.com>
2017-04-03 18:58:05 +0800

    Create Find  in Two-dimensional Arrays.java

---
+++ NowCoder/Sword To Offer/Find  in Two-dimensional Arrays.java
@@ -0,0 +1,23 @@
+/**
+ * 题目描述:
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ **/
+ 
+ public class Solution {
+    public boolean Find(int target, int [][] array) {
+int m=array[0].length;//矩阵的列数
+		int n=array.length;//矩阵的行数
+		int i=m-1,j=0;
+		while(i>=0&&j<=n-1){
+			if(array[j][i]>target){
+				i-=1;
+			}else if(array[j][i]<target)
+			{
+				j+=1;
+			}else if(array[j][i]==target){
+				return true;
+			}
+		}
+		return false;
+    }
+}

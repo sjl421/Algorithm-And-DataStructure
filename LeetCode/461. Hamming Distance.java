f480b2b1c92ce29e32931b8abbe50feeb5f0e814
zhangyong <553899811@qq.com>
2017-04-07 17:19:10 +0800

    update new desc

--- LeetCode/461. Hamming Distance.java
+++ LeetCode/461. Hamming Distance.java
@@ -1,11 +1,10 @@
 /**
- * URL:https://leetcode.com/problems/hamming-distance
+ *URL:https://leetcode.com/problems/hamming-distance
+ *
  *The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
  *Given two integers x and y, calculate the Hamming distance.
- *
  *Note:
  *0 ≤ x, y < 231.
- * 
  *Example:
  *
  *Input: x = 1, y = 4

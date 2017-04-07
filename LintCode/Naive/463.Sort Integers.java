0e78eae59d7d8be046bc7cf8f9ea4729ab775b36
zhangyong <553899811@qq.com>
2017-04-07 21:50:42 +0800

    add a new solution for sortInteger

--- LintCode/Naive/463.Sort Integers.java
+++ LintCode/Naive/463.Sort Integers.java
@@ -21,6 +21,41 @@ public class Solution {
             }
         }
     }
+    //another method -QuickSort
+    public void quickSort(int[] arr, int low, int high) {
+		if (arr == null || arr.length == 0) {
+			return;
+		}
+		if (low >= high) {
+			return;
+		}
+		int middle = low + (high - low) / 2;
+		int val = arr[middle];
+
+		int i = low, j = high;
+		while (i <= j) {
+			while (arr[i] < val) {
+				i++;
+			}
+			while (arr[j] > val) {
+				j--;
+			}
+			if (i <= j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		if (low < j) {
+			quickSort(arr, low, j);
+		}
+		if (high > i) {
+			quickSort(arr, i, high);
+		}
+	}
+
 }
 /**
  * 

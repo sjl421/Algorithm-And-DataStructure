21b679b025c6237530520f23955bbe1ddd149a3e
zhangyong <553899811@qq.com>
2018-04-11 20:26:39 +0800

    添加AtCoder练习跟踪-认真学习以及练习算法与数据结构

---
+++ Algorithms/Problem Solutions/AtCoder/AtCoder Beginner Contest/093/C.java
@@ -0,0 +1,154 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        solver.solve(1, in, out);
+        out.close();
+    }
+     
+    static class TaskC {
+    /**
+     * url:https://abc093.contest.atcoder.jp/tasks/arc094_a
+     * @desc
+     *  A ,B ,C 三个数,现在有2个操作
+     *  [1] 任意两个数 均 +1
+     *  [2] 任意一个数 +2
+     *  求最小让三个数相同的操作数;
+     * @param testNumber
+     * @param in
+     * @param out
+     */
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int[] arr = new int[3];
+            arr[0] = in.nextInt();
+            arr[1] = in.nextInt();
+            arr[2] = in.nextInt();
+            Arrays.sort(arr);
+            int max = arr[2];
+            int second_max = arr[1];
+            int min = arr[0];
+            int sum = 0;
+            int len = max - second_max;
+            sum += len;
+            min += len;
+            if ((max - min) % 2 == 0) {
+                sum += (max - min) / 2;
+            } else {
+                sum += (max - min) / 2 + 2;
+            }
+            out.println(sum);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+}
+
